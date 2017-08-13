3e28ba07771c4333b0782cd3b1f8586561491ee5
DmyP <Pechenyi.D@gmail.com>
2017-08-13 12:50:00 +0300

    + slf4j Logger

--- src/main/java/com/ep/spring/hometask/SpringMain.java
+++ src/main/java/com/ep/spring/hometask/SpringMain.java
@@ -1,19 +1,26 @@
 package com.ep.spring.hometask;
 
+import com.ep.spring.hometask.domain.User;
 import com.ep.spring.hometask.repository.UserRepository;
+import com.ep.spring.hometask.service.UserService;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Iterator;
 
 public class SpringMain {
     public static void main(String[] args) throws IOException {
         ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring.xml");
         System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
 
-        UserRepository userRepository = appCtx.getBean(UserRepository.class);
-        userRepository.getAll();
+
+        UserService userService = appCtx.getBean(UserService.class);
+        userService.save(new User());
+        userService.save(new User());
+        userService.getAll().forEach(System.out::println);
+
         appCtx.close();
     }
 }
--- src/main/java/com/ep/spring/hometask/repository/mock/UserRepositoryMockImpl.java
+++ src/main/java/com/ep/spring/hometask/repository/mock/UserRepositoryMockImpl.java
@@ -2,6 +2,8 @@ package com.ep.spring.hometask.repository.mock;
 
 import com.ep.spring.hometask.domain.User;
 import com.ep.spring.hometask.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -11,15 +13,18 @@ import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 
 public class UserRepositoryMockImpl implements UserRepository {
+    private static final Logger log = LoggerFactory.getLogger(UserRepositoryMockImpl.class);
 
     private Map<Long, User> repository = new ConcurrentHashMap<>();
     private AtomicLong counter = new AtomicLong(0);
 
-    private static final Comparator<User> USER_COMPARATOR = Comparator.comparing(User::getFirstName).thenComparing(User::getLastName);
+    // Enable with real DB
+    // private static final Comparator<User> USER_COMPARATOR = Comparator.comparing(User::getFirstName).thenComparing(User::getLastName);
 
     @Nullable
     @Override
     public User getUserByEmail(@Nonnull String email) {
+        log.info("getUserByEmail {}", email);
         return repository.values().stream()
                 .filter(u -> email.equals(u.getEmail()))
                 .findFirst()
@@ -28,6 +33,7 @@ public class UserRepositoryMockImpl implements UserRepository {
 
     @Override
     public User save(@Nonnull User user) {
+        log.info("save {}", user);
         if (user.getId() == null) {
             user.setId(counter.incrementAndGet());
         }
@@ -37,18 +43,20 @@ public class UserRepositoryMockImpl implements UserRepository {
 
     @Override
     public void remove(@Nonnull User user) {
+        log.info("remove {}", user);
         repository.remove(user.getId());
     }
 
     @Override
     public User getById(@Nonnull Long id) {
+
         return repository.get(id);
     }
 
     @Nonnull
     @Override
     public Collection<User> getAll() {
-        return Collections.unmodifiableCollection(repository.values().stream()
-                        .sorted(USER_COMPARATOR).collect(Collectors.toList()));
+        log.info("getAll");
+        return Collections.unmodifiableCollection(repository.values());
     }
 }
